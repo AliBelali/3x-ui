f7de72fdca902326b19a86a624d94e724d326589
Ali Belali <121201502+AliBelali@users.noreply.github.com>
2024-04-30 16:28:02 +0330

    Update DockerEntrypoint.sh

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
 #ssh server
-sleep 5;touch /run/openrc/softlevel
-sleep 1
-rc-status
-sleep 1
-rc-service sshd start
+systemctl start ssh
 
 # Start fail2ban
 fail2ban-client -x start
 
 # Run x-ui
-exec /app/x-ui
+systemctl start x-ui
