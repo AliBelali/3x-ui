bd823ce0fb6a8d9109217b43e7372ef431e80e3e
Ali Belali <121201502+AliBelali@users.noreply.github.com>
2024-12-30 12:28:49 +0330

    Update DockerEntrypoint.sh

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+#ssh server
+/etc/init.d/ssh start
+
 # Start fail2ban
-fail2ban-client -x start
+systemctl start fail2ban
 
 # Run x-ui
-exec /app/x-ui
+#systemctl start x-ui
+exec /usr/local/x-ui/x-ui
+
+#Keep Container Running
+#tail -f /dev/null
